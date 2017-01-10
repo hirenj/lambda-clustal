fc0650fb5d71181e9f4ed4c78d6065edfb7a2c8a
Hiren Joshi <hirenj@gmail.com>
2017-01-10 23:07:02 +0100

    Add in version checking code

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,11 @@ module.exports = function(grunt) {
 	} catch (e) {
 	}
 
-	grunt.initConfig({
+	var config_options = {
+		'git-describe': {
+			options: {},
+			default: {}
+		},
 		lambda_invoke: {
 			default: {
 				package: 'clustal',
@@ -48,7 +52,78 @@ module.exports = function(grunt) {
 			},
 		},
 
+	};
+
+	config_options['lambda_checkversion'] = config_options['lambda_setversion'] = {};
+	Object.keys(config_options['lambda_deploy']).forEach(function(targ) {
+		config_options['lambda_checkversion'][targ] = true;
+		config_options['lambda_setversion'][targ] = true;
 	});
-	grunt.registerTask('deploy', [ 'env:prod', 'lambda_package', 'lambda_deploy']);
+
+	grunt.initConfig(config_options);
+
+	grunt.registerTask('saveRevision', function() {
+		grunt.event.once('git-describe', function (rev) {
+			grunt.option('gitRevision', rev);
+		});
+		grunt.task.run('git-describe');
+	});
+
+	grunt.registerMultiTask('lambda_checkversion','Check for version of lambda function',function(){
+		grunt.task.requires('git-describe');
+		var done = this.async();
+		var target = this.target;
+		if ( ! grunt.config().lambda_deploy[target] ) {
+			grunt.log.writeln("No arn");
+			grunt.config('lambda_package.'+target,null);
+			grunt.config('lambda_deploy.'+target,null);
+			grunt.config('lambda_setversion.'+target,null);
+			done();
+			return;
+		}
+		var arn = grunt.config().lambda_deploy[target].function;
+		var AWS = require('aws-sdk');
+		var lambda = new AWS.Lambda();
+		lambda.getFunctionConfiguration({FunctionName: arn},function(err,data) {
+			var git_status = grunt.option('gitRevision');
+			if (git_status.dirty) {
+				grunt.log.writeln("Git repo is dirty, updating by default");
+			} else {
+				var current_version = data.Description;
+				if (current_version === git_status.toString()) {
+					grunt.config('lambda_package.'+target,null);
+					grunt.config('lambda_deploy.'+target,null);
+					grunt.config('lambda_setversion.'+target,null);
+				}
+			}
+			done();
+		});
+	});
+
+	grunt.registerMultiTask('lambda_setversion','Set version for lambda function',function(){
+		grunt.task.requires('git-describe');
+		var done = this.async();
+		var target = this.target;
+		if ( ! grunt.config().lambda_deploy[target] ) {
+			grunt.log.writeln("No arn");
+			done();
+			return;
+		}
+		var arn = grunt.config().lambda_deploy[target].function;
+		var AWS = require('aws-sdk');
+		var lambda = new AWS.Lambda();
+		grunt.log.writeln("Setting version for "+target+" to ",grunt.option('gitRevision').toString());
+		lambda.updateFunctionConfiguration({FunctionName: arn,Description: grunt.option('gitRevision').toString() },function(err,data) {
+			if ( ! err ) {
+				done();
+			} else {
+				grunt.fail.fatal(err);
+			}
+		});
+	});
+
+	grunt.registerTask('versioncheck',['saveRevision']);
+
+	grunt.registerTask('deploy', [ 'env:prod', 'versioncheck', 'lambda_checkversion', 'lambda_package', 'lambda_deploy','lambda_setversion']);
 	grunt.registerTask('test', ['lambda_invoke']);
 };
